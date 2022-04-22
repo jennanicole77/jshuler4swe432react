ada5f5e41937f2a84bf29b49cab988575509016e
jennashuler <jenna.shuler@specinnovations.com>
2022-04-21 23:12:41 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -1,64 +1,62 @@
+import React from 'react';
 
-export default function App(props) {
-/*
-    fetch('https://jshuler4swe432.herokuapp.com/assignment8')
-    .then(response => response.text())
-    .then(data => {
-            document.write(res);
-    });
+export default class App extends React.Component {
+ constructor(props) {
+  	super(props)
+        this.refs = React.createRef();
+        this.enterNumbers = this.enterNumbers.bind(this);
+        this.changeFontSizeSlider = this.changeFontSizeSlider.bind(this);
+        this.state = {size:20, innerHtml:""};
+       
+    }
 
-    let numOfChar = 0;
-    var container = document.getElementById("container");
-    let setFocus = function()
+    componentDidMount()
     {
-        document.form.characteristics.focus();
+        this.refs.characteristics.focus();
     }
-    let enterNumbers = function()
+
+    enterNumbers()
     {
-        numOfChar = document.getElementById("characteristics").value;
-        var html = "";
-        for(var i = 1; i<=numOfChar; i++) {
-        html +=
-        "<label for=\"characteristics" + i + "\"> Characteristic " + i + "'s number of blocks: (Please enter a valid number above 0):</label>" +
-        "<input type=\"number\" id=\"characteristics" + i + "\" name=\"characteristics" + i + "\" min=01 required><br><br>";
+        let numOfChar = this.refs.characteristics.value;
+        let html = "";
+        for(let i = 1; i<=numOfChar; i++) {
+            html +=
+            "<label for=\"characteristics" + i + "\"> Characteristic " + i + "'s number of blocks: (Please enter a valid number above 0):</label>" +
+            "<input type=\"number\" id=\"characteristics" + i + "\" name=\"characteristics" + i + "\" min=01 required><br><br>";
+        }
+        this.refs.amountOfInputs.innerHTML = html;
     }
-    document.getElementById("amountOfInputs").innerHTML = html;
-    }
-    let changeFontSizeSlider = function()
+
+    changeFontSizeSlider()
     {
-        var slider = document.getElementById("slider");
-        container.style.fontSize = slider.value;
+        let slider = this.refs.slider;
+        this.setState({size: slider.value, innerHtml:""});  
     }
-*/
-    return (    
-        <div>
-            <h1> Hi </h1>
-        </div>
-  /*
-        <html>
-            <body onload="setFocus()">
-                <h1 style="text-align:center; color:black;">SWE 432 Assignment 8</h1>
-                <h2 style="text-align:center; color:black;">Creators: Jenna Shuler and Frank Costantino</h2>
-                <div style = "text-align:center">
-                    <a href="https://github.com/jennanicole77/jshuler4swe432/blob/main/src/main/java/servlet/Assignment8.java">Assignment 8 Github Link (Back end)</a>
+
+    render() { 
+
+        return (
+            <div>
+                <h1 style={{textAlign: 'center', color: 'black'}}>SWE 432 Assignment 8</h1>
+                <h2 style={{textAlign: 'center', color: 'black'}}>Creators: Jenna Shuler and Frank Costantino</h2>
+                <div style={{textAlign: 'center'}}>
+                    <a href="https://github.com/jennanicole77/jshuler4swe432react/blob/master/src/App.js">Assignment 8 Github Link </a>
                 </div>
-                <div style = "text-align:center">
-                    <h3 style="text-align:center; color:black;">Use the slider below to change the font size of the information below!</h3>
-                    <input type="range" min="15" max="40" id="slider" onChange="changeFontSizeSlider()" value="20"/>
+                <div style={{textAlign: 'center'}}>
+                    <h3 style={{textAlign: 'center', color: 'black'}}>Use the slider below to change the font size of the information below!</h3>
+                    <input type="range" min={15} max={40} id="slider" onChange={this.changeFontSizeSlider} defaultValue={20} ref="slider"/>
                 </div>
-                <form method="post" action="https://jshuler4swe432.herokuapp.com/assignment8" name="form" id="formId">
-                    <div style="font-size:20px;" id="container">
+                <form method="post" action="https://jshuler4swe432.herokuapp.com/assignment8" name="form" id="formId" ref="formId">
+                    <div style={{fontSize: this.state.size+'px'}} id="container" ref="container">
                         <label htmlFor="characteristics">Enter a number of characteristics to be created below (Please enter a valid number above 0):</label>
-                        <input type="number" id="characteristics" name="characteristics" min="1" required onInput="enterNumbers()"/>
-                        <br/><br/>
-                        <div id="amountOfInputs"></div>
+                        <input type="number" id="characteristics" ref="characteristics" name="characteristics" min={1} required onInput={this.enterNumbers} />
+                        <br /><br />
+                        <div id="amountOfInputs" ref="amountOfInputs" />
                         <center>
-                            <input type="submit"  style="color:#37AEE2; font-size: 16px; padding: 1rem 1.75rem; justify-content: center;"/>
+                            <input type="submit" style={{color: '#37AEE2', fontSize: '16px', padding: '1rem 1.75rem', justifyContent: 'center'}} />
                         </center>
                     </div>
                 </form>
-            </body>
-        </html> 
-*/
-    )
-}
+            </div>
+        )
+    }}
